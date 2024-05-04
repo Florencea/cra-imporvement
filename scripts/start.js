ae91e5e8d97c0c53c18eaea68b104068d2f697d0
Florencea <bearflorencea@gmail.com>
2024-05-04 21:44:25 +0800

    chore: bump webpack-dev-server to 5.0.4

--- scripts/start.js
+++ scripts/start.js
@@ -138,7 +138,7 @@ checkBrowsers(paths.appPath, isInteractive)
 
     ["SIGINT", "SIGTERM"].forEach(function (sig) {
       process.on(sig, function () {
-        devServer.close();
+        devServer.stop();
         process.exit();
       });
     });
@@ -146,7 +146,7 @@ checkBrowsers(paths.appPath, isInteractive)
     if (process.env.CI !== "true") {
       // Gracefully exit when stdin ends
       process.stdin.on("end", function () {
-        devServer.close();
+        devServer.stop();
         process.exit();
       });
     }
